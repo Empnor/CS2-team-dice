0de582bbb577c645bd7111d87b7e257cf4fd2a72
Empnor <eemeli.noronen@gmail.com>
2024-01-10 12:41:37 +0200

    Update jamk.py

--- jamk.py
+++ jamk.py
@@ -1,21 +1,21 @@
 import random
 
+
 class jamk:
     while True:
         lol = input("laita komento mitä haluat tehdä")
+        if lol == "":
+            break
         
-        def hae_pelaaja(filename):
-            with open(filename, 'r') as file:
-                return [line.strip() for line in file.readlines()]
-
-# Read players from different files for each role
+        if lol == "arvo":
+            def hae_pelaaja(filename):
+                with open(filename, 'r') as file:
+                    return [line.strip() for line in file.readlines()]
         igllä = hae_pelaaja('igl.txt')
         bossit = hae_pelaaja('awp.txt')
         entryt = hae_pelaaja('entry.txt')
         lurk_players = hae_pelaaja('lurk.txt')
         rifle_players = hae_pelaaja('rifle.txt')
-
-
         #onko pelaajia tarpeeksi joka roolissa
         if len(igllä) >= 1 and len(bossit) >= 1 and len(entryt) >= 1 and len(lurk_players) >= 1 and len(rifle_players) >= 1:
             # valitse rooliin pelaaja niin että se ei toista muita pelaajia
@@ -24,7 +24,6 @@ class jamk:
             selected_entry = random.choice(list(set(entryt) - {selected_igl, selected_awp}))
             selected_lurk = random.choice(list(set(lurk_players) - {selected_igl, selected_awp, selected_entry}))
             selected_rifle = random.choice(list(set(rifle_players) - {selected_igl, selected_awp, selected_entry, selected_lurk}))
-
         # anna roolit pelaajille
         roles = ['IGL', 'AWP', 'Entry', 'Lurk', 'Rifle']
         players_with_roles = {
@@ -34,10 +33,8 @@ class jamk:
             selected_lurk: 'Lurk',
             selected_rifle: 'Rifle'
         }
-
         for player, role in players_with_roles.items():
             print(f"{player}: {role}")
-    else:
-        print("Not enough players in one or more role lists.")
+   
 
 
