e23eaab08517cdd6676308a6bdd12417beb860b5
Empnor <eemeli.noronen@gmail.com>
2024-01-18 08:22:13 +0200

    .

--- jamk.py
+++ jamk.py
@@ -1,43 +1,78 @@
+import PySimpleGUI as sg
+import os
 import random
 
-class jamk:
-    while True:
-        #input
-        inputti = input("laita komento mitä haluat tehdä ")
-        #lopeta sovellus inputti
-        if inputti == "":
-            break
-        #arvo tiimi inputti
-        if inputti == "tiimi":
-            #lukee tiedostot
-            def hae_pelaaja(filename):
-                with open(filename, 'r') as file:
-                    return [line.strip() for line in file.readlines()]
-        #hakee pelaajat 
-        igllä = hae_pelaaja('igl.txt')
-        bossit = hae_pelaaja('awp.txt')
-        entryt = hae_pelaaja('entry.txt')
-        lurkit = hae_pelaaja('lurk.txt')
-        riflet = hae_pelaaja('rifle.txt')
-        
-        #onko pelaajia tarpeeksi joka roolissa
+class Jamk:
+    def __init__(self):
+        # windowin nimi ja layout
+        self.window = sg.Window('tiimien arvonta',
+                                layout=[[sg.Button('tiimin randomisointi', key='Buttonit')],
+                                        [sg.Text('', size=(30, 10), key='roolit')],
+                                        [sg.Image(filename='', key='igl_image', size=(50, 50)),
+                                         sg.Image(filename='', key='awp_image', size=(50, 50)),
+                                         sg.Image(filename='', key='entry_image', size=(50, 50)),
+                                         sg.Image(filename='', key='lurk_image', size=(50, 50)),
+                                         sg.Image(filename='', key='rifle_image', size=(50, 50))]],
+                                margins=(50, 50))
+        self.roolit = ['IGL', 'AWP', 'Entry', 'Lurk', 'Rifle']
+        self.rooli_pelaajat = {}
+
+    # hakee pelaajan
+    def hae_pelaaja(self, filename):
+        with open(filename, 'r') as file:
+            return [line.strip() for line in file.readlines()]
+
+    # valitsee rooliin pelaajan
+    def valitse_rooli(self):
+        igllä = self.hae_pelaaja('igl.txt')
+        bossit = self.hae_pelaaja('awp.txt')
+        entryt = self.hae_pelaaja('entry.txt')
+        lurkit = self.hae_pelaaja('lurk.txt')
+        riflet = self.hae_pelaaja('rifle.txt')
+
+        # ettei samoja nimiä valita moneen rooliin
         if len(igllä) >= 1 and len(bossit) >= 1 and len(entryt) >= 1 and len(lurkit) >= 1 and len(riflet) >= 1:
-            # valitse rooliin pelaaja niin että se ei toista muita pelaajia
             valittu_igl = random.choice(igllä)
             valittu_bossi = random.choice(list(set(bossit) - {valittu_igl}))
             valittu_entry = random.choice(list(set(entryt) - {valittu_igl, valittu_bossi}))
             valittu_lurkki = random.choice(list(set(lurkit) - {valittu_igl, valittu_bossi, valittu_entry}))
-            valittu_rifle = random.choice(list(set(riflet) - {valittu_igl, valittu_bossi, valittu_entry, valittu_lurkki}))
-        # anna roolit pelaajille
-        
-        roles = ['IGL', 'AWP', 'Entry', 'Lurk', 'Rifle']
-        players_with_roles = {
-            valittu_igl: 'IGL',
-            valittu_bossi: 'AWP',
-            valittu_entry: 'Entry',
-            valittu_lurkki: 'Lurk',
-            valittu_rifle: 'Rifle'
-        }
-        #rooli ja pelaaja kirjoitetaan yhteen
-        for player, role in players_with_roles.items():
-            print(f"{player}: {role}")
+            valittu_rifle = random.choice(
+                list(set(riflet) - {valittu_igl, valittu_bossi, valittu_entry, valittu_lurkki}))
+
+            # roolit
+            self.rooli_pelaajat = {
+                'IGL': valittu_igl,
+                'AWP': valittu_bossi,
+                'Entry': valittu_entry,
+                'Lurk': valittu_lurkki,
+                'Rifle': valittu_rifle
+            }
+
+           #näytä pelaajien kuvat
+            for role, player in self.rooli_pelaajat.items():
+                image_path = os.path.join('images', f'{player}.png')
+                self.window[f'{role.lower()}_image'].update(filename=image_path)
+
+    def näytä_rooli(self):
+        roolit_text = ''
+        for role, player in self.rooli_pelaajat.items():
+            roolit_text += f"{role}: {player}\n"
+        self.window['roolit'].update(roolit_text)
+
+    def run(self):
+        while True:
+            event, values = self.window.read()
+
+            if event == sg.WINDOW_CLOSED:
+                break
+
+            if event == 'Buttonit':
+                self.valitse_rooli()
+                self.näytä_rooli()
+
+        self.window.close()
+
+
+if __name__ == "__main__":
+    app = Jamk()
+    app.run()
